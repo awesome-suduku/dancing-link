467e063a0cee4bc0158f7306a2810f9d1e515420
lax <lax263@zrtg.com>
2021-04-16 17:23:55 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@
  * @Author: lax
  * @Date: 2021-04-12 16:44:57
  * @LastEditors: lax
- * @LastEditTime: 2021-04-14 19:26:50
+ * @LastEditTime: 2021-04-16 17:12:21
  * @FilePath: \dancing-links\webpack.config.js
  */
 const path = require("path");
 const resolve = dir => path.resolve(__dirname, dir);
-// const webpack = require("webpack")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
 	devtool: "inline-cheap-source-map",
@@ -19,6 +19,19 @@ module.exports = {
 		filename: "index.js",
 		libraryTarget: "commonjs2",
 		library: "Stage"
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					compress: {
+						warnings: false,
+						drop_debugger: true,
+						drop_console: true
+					}
+				}
+			})
+		]
 	},
 	resolve: {
 		// 设置别名
