ca48db25b84775f771f0d40fdf4388f58fec1007
lax <lax263@zrtg.com>
2021-04-16 16:54:21 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Version: 1.0.0
+ * @Author: lax
+ * @Date: 2021-04-16 16:52:24
+ * @LastEditors: lax
+ * @LastEditTime: 2021-04-16 16:52:46
+ * @FilePath: \dancing-links\src\index.js
+ */
+
+module.exports = require("./Element");
