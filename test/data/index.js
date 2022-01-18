ce3236a69bde08a3dbf4ced8d0da369c314024a9
lax <lax263@zrtg.com>
2022-01-18 11:46:36 +0800

    update

--- test/data/index.js
+++ test/data/index.js
@@ -4,7 +4,7 @@
  * @Author: lax
  * @Date: 2021-04-13 10:05:08
  * @LastEditors: lax
- * @LastEditTime: 2022-01-18 11:17:20
+ * @LastEditTime: 2022-01-18 11:44:20
  * @FilePath: \dancing-links\test\data\index.js
  */
 module.exports = {
@@ -21,6 +21,14 @@ module.exports = {
     { data: "10101", row: [1, 0, 1, 0, 1] },
     { data: "00100", row: [0, 0, 1, 0, 0] },
     { data: "01010", row: [0, 1, 0, 1, 0] }
+  ],
+  three: [
+    { data: "01001", row: [0, 1, 0, 0, 1] },
+    { data: "10010", row: [1, 0, 0, 1, 0] },
+    { data: "10101", row: [1, 0, 1, 0, 1] },
+    { data: "00100", row: [0, 0, 1, 0, 0] },
+    { data: "01010", row: [0, 1, 0, 1, 0] },
+    { data: "01101", row: [0, 1, 1, 0, 1] }
   ],
   zero: [
     { data: "010", row: [0, 1, 0] },
